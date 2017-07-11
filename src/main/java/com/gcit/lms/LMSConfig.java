a52da792c0e2a41e161c8bb4d79139b1eafc34da
ocedanoescalante <ocedanoescalante@email.arizona.edu>
2017-07-11 11:24:48 -0400

    WebHook Test2

--- src/main/java/com/gcit/lms/LMSConfig.java
+++ src/main/java/com/gcit/lms/LMSConfig.java
@@ -21,13 +21,12 @@ import com.gcit.lms.service.AdminService;
 
 @Configuration
 public class LMSConfig extends ResourceConfig {
-
+	
 	public String driver = "com.mysql.jdbc.Driver";
 	public String url = "jdbc:mysql://localhost/library";
 	public String username = "root";
 	public String password = "Hur373612";
 
-	
 	@Bean(destroyMethod = "")
 	public BasicDataSource dataSource() {
 		BasicDataSource ds = new BasicDataSource();
